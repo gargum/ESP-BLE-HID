62226f787565e02f58f755110737ac9ca1b26fa4
Gareth <143119785+gargum@users.noreply.github.com>
2025-10-15 16:25:22 -0400

    Updated the example to include some of the new features

--- examples/SendKeyStrokes/SendKeyStrokes.ino
+++ examples/SendKeyStrokes/SendKeyStrokes.ino
@@ -1,5 +1,5 @@
 /**
- * This example turns the ESP32 into a Bluetooth LE keyboard that writes the words, presses Enter, presses a media key and then Ctrl+Alt+Delete
+ * This example demonstrates a little of what this library can do with the ESP32, even without USB Host Mode
  */
 #include <BleKeyboard.h>
 
@@ -8,7 +8,11 @@ BleKeyboard bleKeyboard;
 void setup() {
   Serial.begin(115200);
   Serial.println("Starting BLE work!");
+  //Name must be set before calling bleKeyboard.begin()
+  bleKeyboard.setName("Keyboard Demo");
   bleKeyboard.begin();
+  bleKeyboard.use6KRO(); //NKRO is turned on by default, 6KRO must be explicitly specified
+  bleKeyboard.setBatteryLevel(100); //Any number can be put here and it will report properly
 }
 
 void loop() {
@@ -19,28 +23,35 @@ void loop() {
     delay(1000);
 
     Serial.println("Sending Enter key...");
-    bleKeyboard.write(KEY_RETURN);
+    bleKeyboard.write(KC_ENT);
 
     delay(1000);
 
     Serial.println("Sending Play/Pause media key...");
-    bleKeyboard.write(KEY_MEDIA_PLAY_PAUSE);
+    bleKeyboard.write(KC_MPLY);
 
     delay(1000);
 
+    Serial.println("Maximizing the screen brightness...");
+    bleKeyboard.press(KC_BRIU);
+    delay(2000);
+    bleKeyboard.release(KC_BRIU);
+
+    delay(1000);
+    
    //
    // Below is an example of pressing multiple keyboard modifiers 
-   // which by default is commented out.
-    /*
-    Serial.println("Sending Ctrl+Alt+Delete...");
-    bleKeyboard.press(KEY_LEFT_CTRL);
-    bleKeyboard.press(KEY_LEFT_ALT);
-    bleKeyboard.press(KEY_DELETE);
+   // which by default is commented out. 
+   // 
+   /* Serial.println("Sending Ctrl+Alt+Delete...");
+    bleKeyboard.press(KC_LCTL);
+    bleKeyboard.press(KC_LALT);
+    bleKeyboard.press(KC_DEL);
     delay(100);
     bleKeyboard.releaseAll();
     */
-  }
 
+  }
   Serial.println("Waiting 5 seconds...");
   delay(5000);
 }
