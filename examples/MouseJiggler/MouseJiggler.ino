3bd39aa3e8df7685febf99bd61793bcdbae893e1
Gareth <143119785+gargum@users.noreply.github.com>
2025-10-16 22:52:03 -0400

    Created MouseJiggler.ino
    Showed why a library like this should exist in under 20 lines

---
+++ examples/MouseJiggler/MouseJiggler.ino
@@ -0,0 +1,26 @@
+/**
+ * This example takes an ESP32, and turns it into a BLE mouse jiggler.
+ *
+ * Boards like the ESP32-C3 Super Mini, which this example was originally tested on, make this especially useful!
+ */
+
+#include <BleKeyboard.h>
+
+BleKeyboard jig("Tim's Airpods", "App1e", 100);
+
+void setup() {
+  jig.begin();
+}
+
+void loop() {
+
+  jig.mouseMove(20,0);
+  delay(50);
+  jig.mouseMove(0,20);
+  delay(50);
+  jig.mouseMove(-20,0);
+  delay(50);
+  jig.mouseMove(0,-20);
+  delay(50);
+
+}
