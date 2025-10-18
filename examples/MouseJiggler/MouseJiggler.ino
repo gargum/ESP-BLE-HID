c2b45326ddb8232d7912b79382017d78db266430
Gareth <143119785+gargum@users.noreply.github.com>
2025-10-18 00:37:35 -0400

    Updated MouseJiggler.ino
    Some pointer features aren't supported on iOS/Android (absolute mode) so the heading on the mouse jiggler example was updated to inform the user that yes, it works on mobile, it's been specifically designed to play nicely with every OS

--- examples/MouseJiggler/MouseJiggler.ino
+++ examples/MouseJiggler/MouseJiggler.ino
@@ -1,5 +1,5 @@
 /**
- * This example takes an ESP32, and turns it into a BLE mouse jiggler.
+ * This example takes an ESP32, and turns it into a BLE mouse jiggler. Designed for use on all operating systems. Tested and working on mobile.
  *
  * Boards like the ESP32-C3 Super Mini, which this example was originally tested on, make this especially useful!
  */
