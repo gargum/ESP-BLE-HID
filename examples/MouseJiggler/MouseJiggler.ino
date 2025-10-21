16888cf634e4903f485bc6fa9919026b9e97171c
Gareth <143119785+gargum@users.noreply.github.com>
2025-10-21 01:42:37 -0400

    Updated the mouse jiggler example
    The mouse jiggler now has a PIN setting by default. This way, if you use this at an office job, your manager cannot simply connect to random unsecured Bluetooth devices to figure out you've brought in a wireless mouse jiggler.

--- examples/MouseJiggler/MouseJiggler.ino
+++ examples/MouseJiggler/MouseJiggler.ino
@@ -1,15 +1,16 @@
 /**
- * This example takes an ESP32, and turns it into a BLE mouse jiggler. Designed for use on all operating systems. Tested and working on mobile.
+ * This example takes an ESP32, and turns it into a PIN-protected BLE mouse jiggler. Designed for use on all operating systems. Tested and working on mobile.
  *
  * Boards like the ESP32-C3 Super Mini, which this example was originally tested on, make this especially useful!
  */
 
 #include <BleKeyboard.h>
 
-BleKeyboard jig("Tim's Airpods", "App1e", 100);
+BleKeyboard jig("Tim's Airpods", "App1e", 100); // Setting name, manufacturer, battery level
 
 void setup() {
   jig.begin();
+  jig.setSecurityPin("013370"); // Setting a PIN to connect
 }
 
 void loop() {
