3cfe98a697d9c909d6ae0c0461ca2c1b427e40d6
Gareth <143119785+gargum@users.noreply.github.com>
2025-10-21 19:52:12 -0400

    Made the mouse jiggler example genuinely evil

--- examples/MouseJiggler/MouseJiggler.ino
+++ examples/MouseJiggler/MouseJiggler.ino
@@ -1,14 +1,17 @@
 /**
- * This example takes an ESP32, and turns it into a PIN-protected BLE mouse jiggler. Designed for use on all operating systems. Tested and working on mobile.
+ * This example takes an ESP32, and turns it into a PIN-protected BLE mouse jiggler that falsely reports itself to be an insulin pump.
+ *
+ * Designed for use on all operating systems. Tested and working on mobile.
  *
  * Boards like the ESP32-C3 Super Mini, which this example was originally tested on, make this especially useful!
  */
 
 #include <BleKeyboard.h>
 
-BleKeyboard jig("Tim's Airpods", "App1e", 100); // Setting name, manufacturer, battery level
+BleKeyboard jig("MiniMed 780G", "Medtronic", 100); // Setting name, manufacturer, battery level
 
 void setup() {
+  jig.setAppearance(INSULIN_PUMP);
   jig.begin();
   jig.setSecurityPin("013370"); // Setting a PIN to connect
 }
