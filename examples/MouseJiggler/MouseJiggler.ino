7cfe9fde9b596800c5eb289f465fa84de3d77b6b
Gareth <143119785+gargum@users.noreply.github.com>
2025-10-27 22:54:41 -0400

    Updated the mouse jiggler example to remove deprecated PIN feature

--- examples/MouseJiggler/MouseJiggler.ino
+++ examples/MouseJiggler/MouseJiggler.ino
@@ -13,9 +13,6 @@ BleKeyboard jig("MiniMed 780G", "Medtronic", 100); // Setting name, manufacturer
 void setup() {
   jig.setAppearance(INSULIN_PUMP);
   jig.begin();
-
-  // You can uncomment this line to intentionally prevent smartphones from being able to pair with the ESP32 at all
-  //jig.setSecurityPin("013370"); // Setting a PIN to connect, commented out by default for compatibility on mobile
 }
 
 void loop() {
