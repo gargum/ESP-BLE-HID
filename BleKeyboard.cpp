9f73c4430ed660df3495b75b0f0cb60b1586c4c5
Gareth <143119785+gargum@users.noreply.github.com>
2025-11-03 19:05:32 -0500

    Modifying for cross-platform support

--- BleKeyboard.cpp
+++ BleKeyboard.cpp
@@ -28,7 +28,7 @@ void _bleKeyboardAutoUpdate() {
   uint32_t currentTime = millis();
   
   // Updating every 10ms currently
-  if (currentTime - lastUpdateTime >= 10) {
+  if (currentTime - lastUpdateTime >= SCAN_INTERVAL) {
     lastUpdateTime = currentTime;
     
     if (_activeBleKeyboardInstance) {
@@ -583,7 +583,7 @@ void BleKeyboard::setManufacturer(std::string deviceManufacturer) {
 
 // Sets the waiting time (in milliseconds) between multiple keystrokes in NimBLE mode.
 void BleKeyboard::setDelay(uint32_t ms) {
-  delay(_delay_ms);
+  _delay_ms = ms;
 }
 
 void BleKeyboard::set_vendor_id(uint16_t vid) { 
@@ -644,145 +644,66 @@ bool BleKeyboard::isNKROEnabled() {
   return _useNKRO;
 }
 
-// The ASCII map exists to facilitate the keyboard's write function
-extern
-const uint8_t _asciimap[128] PROGMEM;
-
-#define SHIFT 0x80
-const uint8_t _asciimap[128] =
+static uint8_t charToKeyCode(char c, bool *needShift)
 {
-	0x00,             // NUL
-	0x00,             // SOH
-	0x00,             // STX
-	0x00,             // ETX
-	0x00,             // EOT
-	0x00,             // ENQ
-	0x00,             // ACK
-	0x00,             // BEL
-	0x2a,	          // BS	Backspace
-	0x2b,		  // TAB	Tab
-	0x28,		  // LF	Enter
-	0x00,             // VT
-	0x00,             // FF
-	0x00,             // CR
-	0x00,             // SO
-	0x00,             // SI
-	0x00,             // DEL
-	0x00,             // DC1
-	0x00,             // DC2
-	0x00,             // DC3
-	0x00,             // DC4
-	0x00,             // NAK
-	0x00,             // SYN
-	0x00,             // ETB
-	0x00,             // CAN
-	0x00,             // EM
-	0x00,             // SUB
-	0x00,             // ESC
-	0x00,             // FS
-	0x00,             // GS
-	0x00,             // RS
-	0x00,             // US
-	0x2c,		  //  ' '
-	0x1e|SHIFT,	  // !
-	0x34|SHIFT,	  // "
-	0x20|SHIFT,    // #
-	0x21|SHIFT,    // $
-	0x22|SHIFT,    // %
-	0x24|SHIFT,    // &
-	0x34,          // '
-	0x26|SHIFT,    // (
-	0x27|SHIFT,    // )
-	0x25|SHIFT,    // *
-	0x2e|SHIFT,    // +
-	0x36,          // ,
-	0x2d,          // -
-	0x37,          // .
-	0x38,          // /
-	0x27,          // 0
-	0x1e,          // 1
-	0x1f,          // 2
-	0x20,          // 3
-	0x21,          // 4
-	0x22,          // 5
-	0x23,          // 6
-	0x24,          // 7
-	0x25,          // 8
-	0x26,          // 9
-	0x33|SHIFT,      // :
-	0x33,          // ;
-	0x36|SHIFT,      // <
-	0x2e,          // =
-	0x37|SHIFT,      // >
-	0x38|SHIFT,      // ?
-	0x1f|SHIFT,      // @
-	0x04|SHIFT,      // A
-	0x05|SHIFT,      // B
-	0x06|SHIFT,      // C
-	0x07|SHIFT,      // D
-	0x08|SHIFT,      // E
-	0x09|SHIFT,      // F
-	0x0a|SHIFT,      // G
-	0x0b|SHIFT,      // H
-	0x0c|SHIFT,      // I
-	0x0d|SHIFT,      // J
-	0x0e|SHIFT,      // K
-	0x0f|SHIFT,      // L
-	0x10|SHIFT,      // M
-	0x11|SHIFT,      // N
-	0x12|SHIFT,      // O
-	0x13|SHIFT,      // P
-	0x14|SHIFT,      // Q
-	0x15|SHIFT,      // R
-	0x16|SHIFT,      // S
-	0x17|SHIFT,      // T
-	0x18|SHIFT,      // U
-	0x19|SHIFT,      // V
-	0x1a|SHIFT,      // W
-	0x1b|SHIFT,      // X
-	0x1c|SHIFT,      // Y
-	0x1d|SHIFT,      // Z
-	0x2f,          // [
-	0x31,          // bslash
-	0x30,          // ]
-	0x23|SHIFT,    // ^
-	0x2d|SHIFT,    // _
-	0x35,          // `
-	0x04,          // a
-	0x05,          // b
-	0x06,          // c
-	0x07,          // d
-	0x08,          // e
-	0x09,          // f
-	0x0a,          // g
-	0x0b,          // h
-	0x0c,          // i
-	0x0d,          // j
-	0x0e,          // k
-	0x0f,          // l
-	0x10,          // m
-	0x11,          // n
-	0x12,          // o
-	0x13,          // p
-	0x14,          // q
-	0x15,          // r
-	0x16,          // s
-	0x17,          // t
-	0x18,          // u
-	0x19,          // v
-	0x1a,          // w
-	0x1b,          // x
-	0x1c,          // y
-	0x1d,          // z
-	0x2f|SHIFT,    // {
-	0x31|SHIFT,    // |
-	0x30|SHIFT,    // }
-	0x35|SHIFT,    // ~
-	0				// DEL
-};
+    *needShift = false;
 
+    if (c >= '0' && c <= '9')            return (c - '0') + 0x27;   // 0x27…0x30
+    if (c == ')')  { *needShift = true;  return 0x27; }            // shift-0
+    if (c == '!')  { *needShift = true;  return 0x1e; }            // shift-1
+    if (c == '@')  { *needShift = true;  return 0x1f; }            // shift-2
+    if (c == '#')  { *needShift = true;  return 0x20; }            // shift-3
+    if (c == '$')  { *needShift = true;  return 0x21; }            // shift-4
+    if (c == '%')  { *needShift = true;  return 0x22; }            // shift-5
+    if (c == '^')  { *needShift = true;  return 0x23; }            // shift-6
+    if (c == '&')  { *needShift = true;  return 0x24; }            // shift-7
+    if (c == '*')  { *needShift = true;  return 0x25; }            // shift-8
+    if (c == '(')  { *needShift = true;  return 0x26; }            // shift-9
 
-uint8_t USBPutChar(uint8_t c);
+    if (c >= 'a' && c <= 'z')            return (c - 'a') + 0x04;   // 0x04…0x1D
+    if (c >= 'A' && c <= 'Z') { *needShift = true;  return (c - 'A') + 0x04; }
+
+    switch (c)
+    {
+    case '\n': case '\r':                return KEY_ENTER;          // 0xB0
+    case '\t':                           return KEY_TAB;            // 0xB3
+    case ' ':                            return KEY_SPACE;          // 0x2C
+    case '-': case '_':
+        if (c == '_') *needShift = true;
+        return KEY_MINUS;                                      // 0x2D
+    case '=': case '+':
+        if (c == '+') *needShift = true;
+        return KEY_EQUAL;                                      // 0x2E
+    case '[': case '{':
+        if (c == '{') *needShift = true;
+        return KEY_LEFT_BRACKET;                               // 0x2F
+    case ']': case '}':
+        if (c == '}') *needShift = true;
+        return KEY_RIGHT_BRACKET;                              // 0x30
+    case '\\': case '|':
+        if (c == '|') *needShift = true;
+        return KEY_BACKSLASH;                                  // 0x31
+    case ';': case ':':
+        if (c == ':') *needShift = true;
+        return KEY_SEMICOLON;                                  // 0x33
+    case '\'': case '"':
+        if (c == '"') *needShift = true;
+        return KEY_APOSTROPHE;                                 // 0x34
+    case '`': case '~':
+        if (c == '~') *needShift = true;
+        return KEY_GRAVE;                                      // 0x35
+    case ',': case '<':
+        if (c == '<') *needShift = true;
+        return KEY_COMMA;                                      // 0x36
+    case '.': case '>':
+        if (c == '>') *needShift = true;
+        return KEY_PERIOD;                                     // 0x37
+    case '/': case '?':
+        if (c == '?') *needShift = true;
+        return KEY_FORWARD_SLASH;                              // 0x38
+    default:                             return 0;                // non-printable
+    }
+}
 
 // press() adds the specified key (printing, non-printing, or modifier)
 // to the persistent key report and sends the report.  Because of the way
@@ -959,10 +880,17 @@ void BleKeyboard::gamepadReleaseAll(void)
     sendGamepadReport();
 }
 
-size_t BleKeyboard::write(uint8_t c) {
-  uint8_t p = press(c);  // Keydown
-  release(c);            // Keyup
-  return p;
+size_t BleKeyboard::write(uint8_t c)
+{
+    bool shift;
+    uint8_t key = charToKeyCode((char)c, &shift);
+    if (key == 0) return 0;                     // character not supported
+
+    if (shift) press(KEY_LEFT_SHIFT);           // hold shift
+    press(key);                                 // key-down
+    release(key);                               // key-up
+    if (shift) release(KEY_LEFT_SHIFT);         // release shift
+    return 1;
 }
 
 size_t BleKeyboard::write(int16_t modifier) {
@@ -978,19 +906,11 @@ size_t BleKeyboard::write(uint16_t mediaKey)
 	return p;
 }
 
-size_t BleKeyboard::write(const uint8_t *buffer, size_t size) {
-	size_t n = 0;
-	while (size--) {
-		if (*buffer != '\r') {
-			if (write(*buffer)) {
-			  n++;
-			} else {
-			  break;
-			}
-		}
-		buffer++;
-	}
-	return n;
+size_t BleKeyboard::write(const uint8_t *buf, size_t len)
+{
+    size_t n = 0;
+    while (len--) n += write(*buf++);
+    return n;
 }
 
 void BleKeyboard::setModifiers(uint8_t modifiers) {
