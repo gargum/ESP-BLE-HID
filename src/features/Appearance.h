8017f3b446e9ba7c3cf8a484cd27807ad496fe1c
Gareth <143119785+gargum@users.noreply.github.com>
2025-11-09 20:05:59 -0500

    Put the appearance feature defs in their own file

---
+++ src/features/Appearance.h
@@ -0,0 +1,48 @@
+//  ------------------------------------------
+// | Appearance Selection Feature - Constants |
+//  ------------------------------------------
+
+// Appearance codes
+
+#define GENERIC_HID           0x03C0
+#define KEYBOARD              0x03C1
+#define MOUSE                 0x03C2
+#define JOYSTICK              0x03C3
+#define GAMEPAD               0x03C4
+#define DIGITIZER             0x03C5
+#define DIGITAL_PEN           0x03C7 
+#define HEADPHONES            0x0943
+#define DISPLAY               0x0141
+#define REMOTE_CONTROL        0x0181
+#define REMOTE_PRESENTATION   0x0182
+#define KEYRING               0x0242
+#define DESKTOP               0x0081
+#define SERVER                0x0082
+#define LAPTOP                0x0083
+#define TABLET                0x0087
+#define PHONE                 0x0041
+#define SMARTWATCH            0x00C2
+#define CYCLING_COMPUTER      0x0481
+#define RUNNING_WALKING       0x044C
+#define WEARABLE              0x0086
+#define WEARABLE_IN_SHOE      0x0441
+#define WEARABLE_ON_SHOE      0x0442
+#define WEARABLE_ON_HIP       0x0443
+#define CLOCK                 0x0104
+#define BARCODE_SCANNER       0x03C8
+#define CARD_READER           0x03C6
+#define OUTDOOR_SPORTS        0x1440 // Idk wtf the intended use case for this one is
+#define LOCATION_DISPLAY      0x1441
+#define LOCATION_POD          0x1443
+#define WEIGHT_SCALE          0x0C81
+#define EAR_THERMOMETER       0x0301
+#define BLOOD_PRESSURE        0x0381
+#define PULSE_OXIMETER        0x0C41
+#define GLUCOSE_METER         0x0412
+#define GLUCOSE_CONTINUOUS    0x0D01
+#define MEDICATION_DELIVERY   0x0D81
+#define INSULIN_PEN           0x0D48
+#define INSULIN_PUMP          0x0D41
+#define WHEELCHAIR            0x0CC1
+#define MOBILITY_SCOOTER      0x0CC2
+#define IOT_GATEWAY           0x008D
