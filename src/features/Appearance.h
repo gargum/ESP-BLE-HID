d34320c85f7be3e426beff9a44d09bef6db9af76
Gareth <143119785+gargum@users.noreply.github.com>
2025-11-10 17:48:48 -0500

    Added new appearances

--- src/features/Appearance.h
+++ src/features/Appearance.h
@@ -2,7 +2,7 @@
 // | Appearance Selection Feature - Constants |
 //  ------------------------------------------
 
-// Appearance codes
+// Appearance codes - Broadly Supported
 
 #define GENERIC_HID           0x03C0
 #define KEYBOARD              0x03C1
@@ -11,14 +11,19 @@
 #define GAMEPAD               0x03C4
 #define DIGITIZER             0x03C5
 #define DIGITAL_PEN           0x03C7 
+#define TOUCHPAD              0x03C9
 #define HEADPHONES            0x0943
+#define MEDIA_PLAYER          0x0281
 #define DISPLAY               0x0141
 #define REMOTE_CONTROL        0x0181
 #define REMOTE_PRESENTATION   0x0182
 #define KEYRING               0x0242
 #define DESKTOP               0x0081
 #define SERVER                0x0082
+#define BLADE_SERVER          0x008A
 #define LAPTOP                0x0083
+#define MINI_PC               0x008E
+#define STICK_PC              0x008F
 #define TABLET                0x0087
 #define PHONE                 0x0041
 #define SMARTWATCH            0x00C2
@@ -28,15 +33,21 @@
 #define WEARABLE_IN_SHOE      0x0441
 #define WEARABLE_ON_SHOE      0x0442
 #define WEARABLE_ON_HIP       0x0443
+#define DOCKING_STATION       0x0088
 #define CLOCK                 0x0104
 #define BARCODE_SCANNER       0x03C8
 #define CARD_READER           0x03C6
+#define SPEED_SENSOR          0x0482
+#define CADENCE_SENSOR        0x0483
+#define POWER_SENSOR          0x0484
 #define OUTDOOR_SPORTS        0x1440 // Idk wtf the intended use case for this one is
 #define LOCATION_DISPLAY      0x1441
 #define LOCATION_POD          0x1443
 #define WEIGHT_SCALE          0x0C81
+#define GLASSES               0x01C1
 #define EAR_THERMOMETER       0x0301
 #define BLOOD_PRESSURE        0x0381
+#define HEARTRATE_MONITOR     0x0342
 #define PULSE_OXIMETER        0x0C41
 #define GLUCOSE_METER         0x0412
 #define GLUCOSE_CONTINUOUS    0x0D01
@@ -46,3 +57,41 @@
 #define WHEELCHAIR            0x0CC1
 #define MOBILITY_SCOOTER      0x0CC2
 #define IOT_GATEWAY           0x008D
+
+// Appearance Codes - Supported by almost nothing
+
+#define SWITCH                0x04C1
+#define MULTI_SWITCH          0x04C2
+#define BUTTON                0x04C3
+#define SLIDER                0x04C4
+#define ROTARY_SWITCH         0x04C5
+#define TOUCH_PANEL           0x04C6
+#define SINGLE_SWITCH         0x04C7
+#define DOUBLE_SWITCH         0x04C8
+#define TRIPLE_SWITCH         0x04C9
+#define BATTERY_SWITCH        0x04CA
+#define PUSH_BUTTON           0x04CC
+#define DIAL                  0x04CD
+#define ACCESS_POINT          0x0501
+#define MESH_DEVICE           0x0502
+#define MESH_NETWORK_PROXY    0x0503
+#define MOTION_SENSOR         0x0541
+#define AIR_QUALITY_SENSOR    0x0542
+#define TEMPERATURE_SENSOR    0x0543
+#define HUMIDITY_SENSOR       0x0544
+#define LEAK_SENSOR           0x0545
+#define SMOKE_SENSOR          0x0546
+#define OCCUPANCY_SENSOR      0x0547
+#define CONTACT_SENSOR        0x0548
+#define CARBON_MONOXIDE       0x0549
+#define CARBON_DIOXIDE        0x054A
+#define AMBIENT_LIGHT         0x054B
+#define ENERGY_SENSOR         0x054C
+#define COLOUR_SENSOR         0x054D
+#define RAIN_SENSOR           0x054E
+#define FIRE_SENSOR           0x054F
+#define WIND_SENSOR           0x0550
+#define PROXIMITY_SENSOR      0x0551
+#define ENERGY_METER          0x0557
+#define FLAME_DETECTOR        0x0558
+#define TIRE_PRESSURE_GAUGE   0x0559
