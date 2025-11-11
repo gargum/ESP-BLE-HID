298b5913c208c2a042614831ad98c1092479efef
Gareth <143119785+gargum@users.noreply.github.com>
2025-11-11 17:34:01 -0500

    Allowing the digitizer and mouse to be toggled independently

--- src/config.h
+++ src/config.h
@@ -1,5 +1,6 @@
-#define KEYBOARD_ENABLE true
-#define MEDIA_ENABLE    true
-#define GEMINIPR_ENABLE true
-#define POINTER_ENABLE  true
-#define GAMEPAD_ENABLE  true
+#define KEYBOARD_ENABLE  true
+#define MEDIA_ENABLE     true
+#define GEMINIPR_ENABLE  true
+#define MOUSE_ENABLE     true
+#define DIGITIZER_ENABLE true
+#define GAMEPAD_ENABLE   true
