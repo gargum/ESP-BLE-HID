2167067794bf0862d37131da3a60dd04cae88cb6
Gareth <143119785+gargum@users.noreply.github.com>
2025-11-23 22:38:59 -0500

    Added OLED and NeoPixel support

--- src/config.h
+++ src/config.h
@@ -4,9 +4,18 @@
  */
 
 #define TRANSPORT        BLE
+
 #define KEYBOARD_ENABLE  true
 #define MEDIA_ENABLE     true
 #define STENO_ENABLE     true
 #define MOUSE_ENABLE     true
 #define DIGITIZER_ENABLE true
 #define GAMEPAD_ENABLE   true
+
+//#define LED_ENABLE       true
+//#define LED_PIN          20
+//#define LED_COUNT        48
+
+//#define OLED_ENABLE      true
+//#define SDA_PIN          8
+//#define SCL_PIN          9
