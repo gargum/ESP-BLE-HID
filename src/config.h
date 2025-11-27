4226b1d7fd67e38d7a9c7a93bde4d550ae62e439
Gareth <143119785+gargum@users.noreply.github.com>
2025-11-26 19:43:22 -0500

    Adding the Spacemouse feature

--- src/config.h
+++ src/config.h
@@ -5,41 +5,42 @@
 
 #define TRANSPORT        BLE
 
-#define KEYBOARD_ENABLE  true
-#define MEDIA_ENABLE     true
-#define STENO_ENABLE     true
-#define MOUSE_ENABLE     true
-#define DIGITIZER_ENABLE true
-#define GAMEPAD_ENABLE   true
+#define KEYBOARD_ENABLE   true
+#define MEDIA_ENABLE      true
+#define STENO_ENABLE      true
+#define MOUSE_ENABLE      true
+#define DIGITIZER_ENABLE  true
+#define GAMEPAD_ENABLE    true
+#define SPACEMOUSE_ENABLE false  // Please note that if the Spacemouse is turned on, the Mouse, Digitizer and Gamepad must be turned off. 
 
-#define LED_ENABLE       false
+#define LED_ENABLE        false
 // #define LED_PIN          20
 // #define LED_COUNT        48
 
-#define OLED_ENABLE      false
+#define OLED_ENABLE       false
 // #define OLED_HEIGHT      64
 // #define OLED_WIDTH       128
 
-#define MCP_ENABLE       false
-#define SHIFT_REGISTERS  false
+#define MCP_ENABLE        false
+#define SHIFT_REGISTERS   false
 
-#define UART_ENABLE      false
+#define UART_ENABLE       false
 // #define TX_PIN           21
 // #define RX_PIN           20
 // #define RTS_PIN          0
 // #define CTS_PIN          1
 
-#define I2C_ENABLE       false
+#define I2C_ENABLE        false
 // #define SDA_PIN          8
 // #define SCL_PIN          9
 
-#define SPI_ENABLE       false
+#define SPI_ENABLE        false
 // #define MISO_PIN         5
 // #define MOSI_PIN         6
 // #define SCK_PIN          4
 // #define CS_PIN           7
 
-#define QSPI_ENABLE      false
+#define QSPI_ENABLE       false
 // #define IO0_PIN          7
 // #define IO1_PIN          2
 // #define IO2_PIN          5
@@ -47,12 +48,12 @@
 // #define CS0_PIN          10
 // #define CLK_PIN          6
 
-#define TWAI_ENABLE      false
+#define TWAI_ENABLE       false
 // #define TWAI_TX_PIN      0
 // #define TWAI_RX_PIN      1
 // #define TWAI_BUS_PIN     3
 // #define TWAI_CLK_PIN     4
 
-#define CAN_BUS_ENABLE   false
+#define CAN_BUS_ENABLE    false
 // #define CAN_TX_PIN       0
 // #define CAN_RX_PIN       1
