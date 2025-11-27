2468aa51af28b6a21f5225580f471ff203b27809
Gareth <143119785+gargum@users.noreply.github.com>
2025-11-27 17:28:48 -0500

    Started the automatic handling for Spacemouse + Other pointers

--- src/SQUIDHID.h
+++ src/SQUIDHID.h
@@ -38,15 +38,15 @@
   #include "features/Spacemouse/Spacemouse.h"
 #endif
 
-#if MOUSE_ENABLE
+#if MOUSE_ENABLE && !SPACEMOUSE_ENABLE
   #include "features/Mouse/Mouse.h"
 #endif
 
-#if DIGITIZER_ENABLE
+#if DIGITIZER_ENABLE && !SPACEMOUSE_ENABLE
   #include "features/Digitizer/Digitizer.h"
 #endif
 
-#if GAMEPAD_ENABLE
+#if GAMEPAD_ENABLE && !SPACEMOUSE_ENABLE
   #include "features/Gamepad/Gamepad.h"
 #endif
 
@@ -72,8 +72,8 @@
 #define SQUIDHID_VERSION_REVISION 4
 
 // Scanning/Polling interval
-#define SCAN_INTERVAL 1
-#define POLL_INTERVAL 250
+#define SCAN_INTERVAL   1
+#define POLL_INTERVAL   250
 
 class SQUIDHID : public Print
     , public TransportCallbacks
@@ -111,15 +111,15 @@ private:
     SQUIDSPACEMOUSE           spacemouse;
   #endif
   
-  #if MOUSE_ENABLE
+  #if MOUSE_ENABLE && !SPACEMOUSE_ENABLE
     SQUIDMOUSE                mouse;
   #endif
   
-  #if DIGITIZER_ENABLE
+  #if DIGITIZER_ENABLE && !SPACEMOUSE_ENABLE
     SQUIDTABLET               digitizer;
   #endif
   
-  #if GAMEPAD_ENABLE
+  #if GAMEPAD_ENABLE && !SPACEMOUSE_ENABLE
     SQUIDGAMEPAD              gamepad;
   #endif
   
@@ -151,124 +151,141 @@ public:
   
   ~SQUIDHID();
   
-  void                        pinMode(uint8_t pin, uint8_t mode);
-  void                        digitalWrite(uint8_t pin, uint8_t value);
-  uint8_t                     digitalRead(uint8_t pin);
+  void        pinMode(uint8_t pin, uint8_t mode);
+  void        digitalWrite(uint8_t pin, uint8_t value);
+  uint8_t     digitalRead(uint8_t pin);
   
   #if MCP_ENABLE
     #if I2C_ENABLE
-      bool                    initializeMCP_I2C(uint8_t i2c_addr = MCP23XXX_ADDR, TwoWire *wire = &Wire);
+      bool    initializeMCP_I2C(uint8_t i2c_addr = MCP23XXX_ADDR, TwoWire *wire = &Wire);
     #endif
     #if SPI_ENABLE
-      bool                    initializeMCP_SPI(uint8_t cs_pin, SPIClass *theSPI = &SPI, uint8_t hw_addr = 0x00);
+      bool    initializeMCP_SPI(uint8_t cs_pin, SPIClass *theSPI = &SPI, uint8_t hw_addr = 0x00);
     #endif
-    bool                      isMCPInitialized() const { return mcpInitialized; }
-    MCP23XXX*                 getMCP() { return mcpExpander; }
+    bool      isMCPInitialized() const { return mcpInitialized; }
+    MCP23XXX* getMCP() { return mcpExpander; }
   #endif
   
-  uint16_t                    appearance = KEYBOARD;
-  std::string                 deviceName;
-  std::string                 deviceManufacturer;
-  uint8_t                     batteryLevel;
+  uint16_t    appearance = KEYBOARD;
+  std::string deviceName;
+  std::string deviceManufacturer;
+  uint8_t     batteryLevel;
   
-  void onConnect() override;
-  void onDisconnect() override;
-  void onDataReceived(const uint8_t* data, size_t length) override;
+  void        onConnect() override;
+  void        onDisconnect() override;
+  void        onDataReceived(const uint8_t* data, size_t length) override;
   
-  void begin(const squid_matrix& matrix, const std::vector<std::vector<LayerKeymapEntry>>& layers);
-  void begin(void);
-  void update(void);
-  void end(void);
+  void        begin(const squid_matrix& matrix, const std::vector<std::vector<LayerKeymapEntry>>& layers);
+  void        begin(void);
+  void        update(void);
+  void        end(void);
 
   // Transport management
-  void setTransport(std::unique_ptr<Transport> newTransport);
-  Transport* getTransport() { return transport.get(); }
+  void        setTransport(std::unique_ptr<Transport> newTransport);
+  Transport*  getTransport() { return transport.get(); }
 
-  void setAppearance(uint16_t newAppearance);
+  void        setAppearance(uint16_t newAppearance);
   
-  size_t write(const uint8_t *buffer, size_t size);
-  void   releaseAll();
+  size_t      write(const uint8_t *buffer, size_t size);
+  void        releaseAll();
   
   // BLE helper functions
-  bool isConnected(void);
-  void pollConnection(void);
-  void setBatteryLevel(uint8_t level);
-  void setName(std::string deviceName);  
-  void setManufacturer(std::string deviceManufacturer);
-  void setDelay(uint32_t ms);
+  bool        isConnected(void);
+  void        pollConnection(void);
+  void        setBatteryLevel(uint8_t level);
+  void        setName(std::string deviceName);  
+  void        setManufacturer(std::string deviceManufacturer);
+  void        setDelay(uint32_t ms);
 
   // Misc hardware helper functions
-  void setVendorId(uint16_t vid);
-  void setProductId(uint16_t pid);
-  void setVersion(uint16_t version);
+  void        setVendorId(uint16_t vid);
+  void        setProductId(uint16_t pid);
+  void        setVersion(uint16_t version);
   
   // Matrix and Keymap methods
-  void    setupMatrix(const squid_matrix& matrix);
-  void    setupKeymap(const std::vector<std::vector<LayerKeymapEntry>>& layers);
-  void    updateMatrix();
-  bool    isKeyPressed(size_t switch_index);
-  void    setDefaultLayer(uint8_t layer);
-  void    momentaryLayer(uint8_t layer, bool pressed);
-  void    toggleLayer(uint8_t layer);
-  uint8_t getActiveLayer() const;
-  bool    isLayerActive(uint8_t layer) const;
+  void        setupMatrix(const squid_matrix& matrix);
+  void        setupKeymap(const std::vector<std::vector<LayerKeymapEntry>>& layers);
+  void        updateMatrix();
+  bool        isKeyPressed(size_t switch_index);
+  void        setDefaultLayer(uint8_t layer);
+  void        momentaryLayer(uint8_t layer, bool pressed);
+  void        toggleLayer(uint8_t layer);
+  uint8_t     getActiveLayer() const;
+  bool        isLayerActive(uint8_t layer) const;
     
   #if KEYBOARD_ENABLE
-    size_t  press(NKROKey k);
-    size_t  press(ModKey modifier);
-    size_t  release(NKROKey k);
-    size_t  release(ModKey modifier);
-    size_t  write(uint8_t c);
-    size_t  write(ModKey modifier);
-    void    useNKRO(bool state = enabled);
-    void    use6KRO(bool state = enabled);
-    bool    isNKROEnabled();
-    void    setModifiers(ModKey modifiers);
-    uint8_t getModifiers();
-    void    sendNKROReport();
+    size_t    press(NKROKey k);
+    size_t    press(ModKey modifier);
+    size_t    release(NKROKey k);
+    size_t    release(ModKey modifier);
+    size_t    write(uint8_t c);
+    size_t    write(ModKey modifier);
+    void      useNKRO(bool state = enabled);
+    void      use6KRO(bool state = enabled);
+    bool      isNKROEnabled();
+    void      setModifiers(ModKey modifiers);
+    uint8_t   getModifiers();
+    void      sendNKROReport();
   #endif
   
   #if MEDIA_ENABLE
-    size_t press(MediaKey mediaKey);
-    size_t release(MediaKey mediaKey);
-    size_t write(MediaKey mediaKey);
-    void   sendMediaReport();
+    size_t    press(MediaKey mediaKey);
+    size_t    release(MediaKey mediaKey);
+    size_t    write(MediaKey mediaKey);
+    void      sendMediaReport();
   #endif
   
   #if SPACEMOUSE_ENABLE
-    void move(int16_t tx, int16_t ty, int16_t tz, int16_t rx, int16_t ry, int16_t rz);
-    void translate(int16_t tx, int16_t ty, int16_t tz);
-    void rotate(int16_t rx, int16_t ry, int16_t rz);
-    void press(uint8_t button);
-    void release(uint8_t button);
-    bool spacemouseIsPressed(uint8_t button);
-    void spacemouseSetAllButtons(uint32_t buttons);
-    void sendSpacemouseReport();
-  #endif
+    void      move(int16_t tx, int16_t ty, int16_t tz, int16_t rx, int16_t ry, int16_t rz);
+    void      translate(int16_t tx, int16_t ty, int16_t tz);
+    void      rotate(int16_t rx, int16_t ry, int16_t rz);
+    void      press(SpacemouseKey button);
+    void      release(SpacemouseKey button);
+    bool      spacemouseIsPressed(SpacemouseKey button);
+    void      spacemouseSetAllButtons(uint32_t buttons);
+    void      sendSpacemouseReport();
+    
+    #if MOUSE_ENABLE
+    
+    #endif
+    
+    #if DIGITIZER_ENABLE
+    void      click(uint16_t x, uint16_t y, SpacemouseKey b = DI_BTN1);
+    void      moveTo(uint16_t x, uint16_t y, uint8_t pressure = 0, SpacemouseKey buttons = SpacemouseKey{0});
+    void      beginStroke(uint16_t x, uint16_t y, uint16_t initialPressure = 1270);
+    void      updateStroke(uint16_t x, uint16_t y, uint16_t pressure);
+    void      endStroke(uint16_t x, uint16_t y);
+    void      setDigitizerRange(uint16_t maxX, uint16_t maxY);
+    #endif
+    
+    #if GAMEPAD_ENABLE
+    
+    #endif
+  #else
   
-  #if MOUSE_ENABLE
-    size_t press(MouseKey b = MO_BTN1);
-    size_t release(MouseKey b = MO_BTN1);
-    void   click(MouseKey b = MO_BTN1);
-    void   move(signed char x, signed char y, signed char wheel = 0, signed char hWheel = 0);
-    bool   mouseIsPressed(MouseKey b = MO_BTN1);
-  #endif
+    #if MOUSE_ENABLE
+    size_t    press(MouseKey b = MO_BTN1);
+    size_t    release(MouseKey b = MO_BTN1);
+    void      click(MouseKey b = MO_BTN1);
+    void      move(signed char x, signed char y, signed char wheel = 0, signed char hWheel = 0);
+    bool      mouseIsPressed(MouseKey b = MO_BTN1);
+    #endif
   
-  #if DIGITIZER_ENABLE
-    void click(uint16_t x, uint16_t y, DigitizerKey b = DI_BTN1);
-    void moveTo(uint16_t x, uint16_t y, uint8_t pressure = 0, DigitizerKey buttons = DigitizerKey{0});
-    void beginStroke(uint16_t x, uint16_t y, uint16_t initialPressure = 127);
-    void updateStroke(uint16_t x, uint16_t y, uint16_t pressure);
-    void endStroke(uint16_t x, uint16_t y);
-    void useAbsoluteMode(bool state = true);
-    bool isAbsoluteMode();
-    void useAutoMode(bool state = true);
-    void setDigitizerRange(uint16_t maxX, uint16_t maxY);
-    bool isAutoModeEnabled();
-    void sendDigitizerReport();
-  #endif
+    #if DIGITIZER_ENABLE
+    void      click(uint16_t x, uint16_t y, DigitizerKey b = DI_BTN1);
+    void      moveTo(uint16_t x, uint16_t y, uint8_t pressure = 0, DigitizerKey buttons = DigitizerKey{0});
+    void      beginStroke(uint16_t x, uint16_t y, uint16_t initialPressure = 127);
+    void      updateStroke(uint16_t x, uint16_t y, uint16_t pressure);
+    void      endStroke(uint16_t x, uint16_t y);
+    void      useAbsoluteMode(bool state = true);
+    bool      isAbsoluteMode();
+    void      useAutoMode(bool state = true);
+    void      setDigitizerRange(uint16_t maxX, uint16_t maxY);
+    bool      isAutoModeEnabled();
+    void      sendDigitizerReport();
+    #endif
   
-  #if GAMEPAD_ENABLE
+    #if GAMEPAD_ENABLE
     size_t    press(GamepadButton button);
     size_t    release(GamepadButton button);
     bool      gamepadIsPressed(GamepadButton button);
@@ -281,8 +298,9 @@ public:
     int16_t   gamepadGetAxis(GamepadAnalogue axis);
     void      gamepadSetAllAxes(int16_t values[GAMEPAD_ANALOGUE_COUNT]);
     void      sendGamepadReport();
-  #endif     
-            
+    #endif     
+  #endif
+    
   #if STENO_ENABLE
     size_t    press(StenoKey stenoKey);
     size_t    release(StenoKey stenoKey);
@@ -313,39 +331,39 @@ public:
                        uint_fast8_t height = OLED_HEIGHT,
                        OLED::tDisplayCtrl displayCtrl = SSD1306, 
                        uint8_t i2c_address = 0x3C);
-    OLED* getOLED() { return oledDisplay; }
-    bool isOLEDInitialized() { return oledInitialized; }
-    void oledClear(OLED::tColor color = OLED::BLACK);
-    void oledDisplayUpdate();
-    void oledSetPower(bool enable);
-    void oledSetCursor(uint_fast8_t x, uint_fast8_t y);
-    void oledDrawString(uint_fast8_t x, uint_fast8_t y, const char* s, OLED::tFontScaling scaling = OLED::NORMAL_SIZE, OLED::tColor color = OLED::WHITE);
-    void oledDrawString_P(uint_fast8_t x, uint_fast8_t y, const char* s, OLED::tFontScaling scaling = OLED::NORMAL_SIZE, OLED::tColor color = OLED::WHITE);
-    size_t oledPrintf(uint_fast8_t x, uint_fast8_t y, const char *format, ...);
-    size_t oledPrintf(const char *format, ...);
-    void oledSetTTYMode(bool enabled);
-    void oledDrawBitmap(uint_fast8_t x, uint_fast8_t y, uint_fast8_t width, uint_fast8_t height, const uint8_t* data, OLED::tColor color = OLED::WHITE);
-    void oledDrawBitmap_P(uint_fast8_t x, uint_fast8_t y, uint_fast8_t width, uint_fast8_t height, const uint8_t* data, OLED::tColor color = OLED::WHITE);
-    void oledShowSquidLogo(OLED::tColor color = OLED::WHITE);
-    void oledShowConnectionStatus(bool connected);
-    void oledShowBatteryLevel(uint8_t level);
-    void oledShowLayerInfo(uint8_t layer);
-    void markOLEDDirty() { oledDirty = true; }
+    OLED*     getOLED() { return oledDisplay; }
+    bool      isOLEDInitialized() { return oledInitialized; }
+    void      oledClear(OLED::tColor color = OLED::BLACK);
+    void      oledDisplayUpdate();
+    void      oledSetPower(bool enable);
+    void      oledSetCursor(uint_fast8_t x, uint_fast8_t y);
+    void      oledDrawString(uint_fast8_t x, uint_fast8_t y, const char* s, OLED::tFontScaling scaling = OLED::NORMAL_SIZE, OLED::tColor color = OLED::WHITE);
+    void      oledDrawString_P(uint_fast8_t x, uint_fast8_t y, const char* s, OLED::tFontScaling scaling = OLED::NORMAL_SIZE, OLED::tColor color = OLED::WHITE);
+    size_t    oledPrintf(uint_fast8_t x, uint_fast8_t y, const char *format, ...);
+    size_t    oledPrintf(const char *format, ...);
+    void      oledSetTTYMode(bool enabled);
+    void      oledDrawBitmap(uint_fast8_t x, uint_fast8_t y, uint_fast8_t width, uint_fast8_t height, const uint8_t* data, OLED::tColor color = OLED::WHITE);
+    void      oledDrawBitmap_P(uint_fast8_t x, uint_fast8_t y, uint_fast8_t width, uint_fast8_t height, const uint8_t* data, OLED::tColor color = OLED::WHITE);
+    void      oledShowSquidLogo(OLED::tColor color = OLED::WHITE);
+    void      oledShowConnectionStatus(bool connected);
+    void      oledShowBatteryLevel(uint8_t level);
+    void      oledShowLayerInfo(uint8_t layer);
+    void      markOLEDDirty() { oledDirty = true; }
     #else
       #error "You need to turn on I2C to use the I2C OLED driver!"
     #endif
   #endif
 
-    LogLevel getLogLevel() const;
-    void     setLogLevel(LogLevel level);
-    void     initialize(std::function<void(const LogEntry&)> handler = nullptr);
-    void     log(LogLevel level, const std::string& tag, const std::string& message);
-    void     processQueue();
-    void     flush();
-    void     setMaxQueueSize(uint32_t size);
-    size_t   getQueueSize() const;
-    bool     isInitialized() const;
-    bool     isQueueEmpty() const;
+    LogLevel  getLogLevel() const;
+    void      setLogLevel(LogLevel level);
+    void      initialize(std::function<void(const LogEntry&)> handler = nullptr);
+    void      log(LogLevel level, const std::string& tag, const std::string& message);
+    void      processQueue();
+    void      flush();
+    void      setMaxQueueSize(uint32_t size);
+    size_t    getQueueSize() const;
+    bool      isInitialized() const;
+    bool      isQueueEmpty() const;
     
     // Platform-specific control methods
     #if defined(SQUIDHID_PLATFORM_ESP32)
