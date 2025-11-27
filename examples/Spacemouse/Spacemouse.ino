e13743a67eb136bd5bddbd2bde7d55615535b289
Gareth <143119785+gargum@users.noreply.github.com>
2025-11-26 21:07:32 -0500

    Updated the Spacemouse example to use the cleaner updated macros

--- examples/Spacemouse/Spacemouse.ino
+++ examples/Spacemouse/Spacemouse.ino
@@ -12,7 +12,7 @@ SQUIDHID space("Spinner", "SquidHID", 100);
 
 void setup() {
   space.setLogLevel(LOGGER_VERBOSE);
-  space.setAppearance(TABLET);
+  space.setAppearance(DIGITIZER);
 
   space.begin();
 }
@@ -26,16 +26,16 @@ void loop() {
 
 void spin() {
 
-  space.spacemousePress(SM_01);                 // The keys SM_01 to SM_32 are the codes for the 32 3DConnexion buttons! All 32 of them can even be included in keymaps!
-  space.spacemouseTranslate(100, -50, 200);     // The "spacemouseTranslate" helper allows you to define a linear motion in the X, Y, and Z axes respectively.
+  space.press(SM_01);                 // The keys SM_01 to SM_32 are the codes for the 32 3DConnexion buttons! All 32 of them can even be included in keymaps!
+  space.translate(100, -50, 200);     // The "translate" helper allows you to define a linear motion in the X, Y, and Z axes respectively.
   delay(1000);
-  space.spacemouseTranslate(-100, 50, -200);    // Remember, these inputs use absolute coordinates from -32768 to 32768
+  space.translate(-100, 50, -200);    // Remember, these inputs use absolute coordinates from -32768 to 32768
   delay(1000);
-  space.spacemouseRotate(-500, 100, 300);       // The "spacemouseRotate" helper allows you to define a rotational motion along the X, Y, and Z axes respectively.
+  space.rotate(-500, 100, 300);       // The "rotate" helper allows you to define a rotational motion along the X, Y, and Z axes respectively.
   delay(1000);
-  space.spacemouseRotate(500, -100, -300);      // Just like translations, these inputs use absolute coordinates from -32768 to 32768
+  space.rotate(500, -100, -300);      // Just like translations, these inputs use absolute coordinates from -32768 to 32768
   delay(1000);
-  space.spacemouseRelease(SM_01);               // Remember, "spacemousePress" causes a 3DConnexion key to be held down. To release it, one must call "spacemouseRelease".
+  space.release(SM_01);               // Remember, "press" causes a 3DConnexion key to be held down. To release it, one must call "release".
   delay(1000);
   
 }
