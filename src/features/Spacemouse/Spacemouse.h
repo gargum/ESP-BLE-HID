fd778323da396813ab85d87b03aa6c408aed9aba
Gareth <143119785+gargum@users.noreply.github.com>
2025-11-27 00:25:06 -0500

    Code optimization

--- src/features/Spacemouse/Spacemouse.h
+++ src/features/Spacemouse/Spacemouse.h
@@ -1,160 +1,177 @@
 /**
- * @file Spacemouse.h
- * @brief 3DConnexion Spacemouse feature header
+ * @file Spacemouse.cpp
+ * @brief Implementation of the 3DConnexion Spacemouse feature
  */
- 
-#ifndef SPACEMOUSE_H
-#define SPACEMOUSE_H
 
-#include <stdint.h>
-#include "HIDTypes.h"
-#include "../../drivers/Software/Log/Log.h"
-#include "../../drivers/Software/Event/Types.h"
-#include "../../drivers/Software/Transport/Transport.h"
+#include "Spacemouse.h"
 
-#define SPACETRANS_ID 0x04 // 3DConnexion, why tf did you give one thing 3 report IDs?!?!
-#define SPACEROTAT_ID 0x05 // Look at any gamepad's HID Report you guys, you don't have to do this
-#define SPACECLICK_ID 0x06 // Why would you do it like this?! 
+static const char* SPACEMOUSE_TAG = "SQUID6DOF";
 
-// Spacemouse report structures
-typedef struct {
-    int16_t tx;  // Translation X
-    int16_t ty;  // Translation Y  
-    int16_t tz;  // Translation Z
-} SpaceTranslationReport;
+SQUIDSPACEMOUSE::SQUIDSPACEMOUSE() 
+    : transport(nullptr), _delay_ms(7) {
+    memset(&_transReport, 0, sizeof(_transReport));
+    memset(&_rotReport, 0, sizeof(_rotReport));
+    memset(&_buttonReport, 0, sizeof(_buttonReport));
+}
 
-typedef struct {
-    int16_t rx;  // Rotation X
-    int16_t ry;  // Rotation Y
-    int16_t rz;  // Rotation Z
-} SpaceRotationReport;
-
-typedef struct {
-    uint32_t buttons;  // 32 bits for button bitmask
-} SpaceButtonReport;
-
-static const uint8_t _spacemouseReportDescriptor[] = {
-  // Spacemouse Translation axis
-  USAGE_PAGE(1),       0x01,                      USAGE(1),            0x08,                      
-  COLLECTION(1),       0x01,                      COLLECTION(1),       0x00,                    
-  REPORT_ID(1),        SPACETRANS_ID,             LOGICAL_MINIMUM(2),  0x00, 0x80,            
-  LOGICAL_MAXIMUM(2),  0xFF, 0x7F,                PHYSICAL_MINIMUM(2), 0x00, 0x80,           
-  PHYSICAL_MAXIMUM(2), 0xFF, 0x7F,                USAGE(1),            0x30,                    
-  USAGE(1),            0x31,                      USAGE(1),            0x32,                    
-  REPORT_SIZE(1),      0x10,                      REPORT_COUNT(1),     0x03,                    
-  HIDINPUT(1),         0x02,                      END_COLLECTION(0),
-  // Spacemouse Rotation axis
-  COLLECTION(1),       0x00,                      REPORT_ID(1),        SPACEROTAT_ID,
-  LOGICAL_MINIMUM(2),  0x00, 0x80,                LOGICAL_MAXIMUM(2),  0xFF, 0x7F,            
-  PHYSICAL_MINIMUM(2), 0x00, 0x80,                PHYSICAL_MAXIMUM(2), 0xFF, 0x7F,          
-  USAGE(1),            0x33,                      USAGE(1),            0x34,                    
-  USAGE(1),            0x35,                      REPORT_SIZE(1),      0x10,                    
-  REPORT_COUNT(1),     0x03,                      HIDINPUT(1),         0x02,                    
-  END_COLLECTION(0),
-  // Spacemouse/3DConnexion Buttons (I added 32 of them)
-  COLLECTION(1),       0x00,                      REPORT_ID(1),        SPACECLICK_ID,
-  LOGICAL_MINIMUM(1),  0x00,                      LOGICAL_MAXIMUM(1),  0x01,                  
-  REPORT_SIZE(1),      0x01,                      REPORT_COUNT(1),     0x20,                    
-  USAGE_PAGE(1),       0x09,                      USAGE_MINIMUM(1),    0x01,                    
-  USAGE_MAXIMUM(1),    0x20,                      HIDINPUT(1),         0x02,                    
-  END_COLLECTION(0),                              END_COLLECTION(0),
-};
-
-enum class SpacemouseKeys : uint32_t {
-  SPACEMOUSE_1  = 0x00000001,
-  SPACEMOUSE_2  = 0x00000002,
-  SPACEMOUSE_3  = 0x00000004,
-  SPACEMOUSE_4  = 0x00000008,
-  SPACEMOUSE_5  = 0x00000010,
-  SPACEMOUSE_6  = 0x00000020,
-  SPACEMOUSE_7  = 0x00000040,
-  SPACEMOUSE_8  = 0x00000080,
-  SPACEMOUSE_9  = 0x00000100,
-  SPACEMOUSE_10 = 0x00000200,
-  SPACEMOUSE_11 = 0x00000400,
-  SPACEMOUSE_12 = 0x00000800,
-  SPACEMOUSE_13 = 0x00001000,
-  SPACEMOUSE_14 = 0x00002000,
-  SPACEMOUSE_15 = 0x00004000,
-  SPACEMOUSE_16 = 0x00008000,
-  SPACEMOUSE_17 = 0x00010000,
-  SPACEMOUSE_18 = 0x00020000,
-  SPACEMOUSE_19 = 0x00040000,
-  SPACEMOUSE_20 = 0x00080000,
-  SPACEMOUSE_21 = 0x00100000,
-  SPACEMOUSE_22 = 0x00200000,
-  SPACEMOUSE_23 = 0x00400000,
-  SPACEMOUSE_24 = 0x00800000,
-  SPACEMOUSE_25 = 0x01000000,
-  SPACEMOUSE_26 = 0x02000000,
-  SPACEMOUSE_27 = 0x04000000,
-  SPACEMOUSE_28 = 0x08000000,
-  SPACEMOUSE_29 = 0x10000000,
-  SPACEMOUSE_30 = 0x20000000,
-  SPACEMOUSE_31 = 0x40000000,
-  SPACEMOUSE_32 = 0x80000000
-};
-
-MK(SpacemouseKey, SM_01, SPACEMOUSE_1);
-MK(SpacemouseKey, SM_02, SPACEMOUSE_2);
-MK(SpacemouseKey, SM_03, SPACEMOUSE_3);
-MK(SpacemouseKey, SM_04, SPACEMOUSE_4);
-MK(SpacemouseKey, SM_05, SPACEMOUSE_5);
-MK(SpacemouseKey, SM_06, SPACEMOUSE_6);
-MK(SpacemouseKey, SM_07, SPACEMOUSE_7);
-MK(SpacemouseKey, SM_08, SPACEMOUSE_8);
-MK(SpacemouseKey, SM_09, SPACEMOUSE_9);
-MK(SpacemouseKey, SM_10, SPACEMOUSE_10);
-MK(SpacemouseKey, SM_11, SPACEMOUSE_11);
-MK(SpacemouseKey, SM_12, SPACEMOUSE_12);
-MK(SpacemouseKey, SM_13, SPACEMOUSE_13);
-MK(SpacemouseKey, SM_14, SPACEMOUSE_14);
-MK(SpacemouseKey, SM_15, SPACEMOUSE_15);
-MK(SpacemouseKey, SM_16, SPACEMOUSE_16);
-MK(SpacemouseKey, SM_17, SPACEMOUSE_17);
-MK(SpacemouseKey, SM_18, SPACEMOUSE_18);
-MK(SpacemouseKey, SM_19, SPACEMOUSE_19);
-MK(SpacemouseKey, SM_20, SPACEMOUSE_20);
-MK(SpacemouseKey, SM_21, SPACEMOUSE_21);
-MK(SpacemouseKey, SM_22, SPACEMOUSE_22);
-MK(SpacemouseKey, SM_23, SPACEMOUSE_23);
-MK(SpacemouseKey, SM_24, SPACEMOUSE_24);
-MK(SpacemouseKey, SM_25, SPACEMOUSE_25);
-MK(SpacemouseKey, SM_26, SPACEMOUSE_26);
-MK(SpacemouseKey, SM_27, SPACEMOUSE_27);
-MK(SpacemouseKey, SM_28, SPACEMOUSE_28);
-MK(SpacemouseKey, SM_29, SPACEMOUSE_29);
-MK(SpacemouseKey, SM_30, SPACEMOUSE_30);
-MK(SpacemouseKey, SM_31, SPACEMOUSE_31);
-MK(SpacemouseKey, SM_32, SPACEMOUSE_32);
-
-class SQUIDSPACEMOUSE {
-private:
-    Transport*              transport;
-    SpaceTranslationReport  _transReport;
-    SpaceRotationReport     _rotReport;  
-    SpaceButtonReport       _buttonReport;
-    uint32_t                _delay_ms;
+void SQUIDSPACEMOUSE::begin(Transport* trans, uint32_t delay_ms) {
+    transport = trans;
+    _delay_ms = delay_ms;
+    memset(&_transReport, 0, sizeof(_transReport));
+    memset(&_rotReport, 0, sizeof(_rotReport));
+    memset(&_buttonReport, 0, sizeof(_buttonReport));
     
-public:
-    SQUIDSPACEMOUSE();
-    
-    void   begin(Transport* transport, uint32_t delay_ms = 7);
-    bool   isConnected();
-    void   onConnect();
-    void   onDisconnect();
-    
-    // Space Mouse methods
-    void   move(int16_t tx, int16_t ty, int16_t tz, int16_t rx, int16_t ry, int16_t rz);
-    void   translate(int16_t tx, int16_t ty, int16_t tz);
-    void   rotate(int16_t rx, int16_t ry, int16_t rz);
-    void   press(uint8_t button);
-    void   release(uint8_t button);
-    bool   isPressed(uint8_t button);
-    void   setAllButtons(uint32_t buttons);
-    void   sendReport();
-    void   releaseAll();
-    
-};
+    SQUID_LOG_DEBUG(SPACEMOUSE_TAG, "Spacemouse subsystem initialized with delay: %lu ms", delay_ms);
+    SQUID_LOG_INFO(SPACEMOUSE_TAG, "Spacemouse service ready");
+}
 
-#endif
+bool SQUIDSPACEMOUSE::isConnected() {
+    return transport ? transport->isConnected() : false;
+}
+
+void SQUIDSPACEMOUSE::onConnect() {
+    SQUID_LOG_DEBUG(SPACEMOUSE_TAG, "Spacemouse connected");
+}
+
+void SQUIDSPACEMOUSE::onDisconnect() {
+    SQUID_LOG_DEBUG(SPACEMOUSE_TAG, "Spacemouse disconnected");
+}
+
+void SQUIDSPACEMOUSE::move(int16_t tx, int16_t ty, int16_t tz, int16_t rx, int16_t ry, int16_t rz) {
+    if (isConnected() && transport) {
+        // Set translation values
+        _transReport.tx = tx;
+        _transReport.ty = ty;
+        _transReport.tz = tz;
+        
+        // Set rotation values
+        _rotReport.rx = rx;
+        _rotReport.ry = ry;
+        _rotReport.rz = rz;
+        
+        SQUID_LOG_DEBUG(SPACEMOUSE_TAG, 
+            "Spacemouse movement - T:(%d, %d, %d) R:(%d, %d, %d)", tx, ty, tz, rx, ry, rz);
+        
+        sendReport();
+    } else {
+        SQUID_LOG_DEBUG(SPACEMOUSE_TAG, "Spacemouse movement ignored - %s", 
+                     !isConnected() ? "not connected" : "no transport");
+    }
+}
+
+void SQUIDSPACEMOUSE::translate(int16_t tx, int16_t ty, int16_t tz) {
+    if (isConnected() && transport) {
+        // Set translation values
+        _transReport.tx = tx;
+        _transReport.ty = ty;
+        _transReport.tz = tz;
+        
+        SQUID_LOG_DEBUG(SPACEMOUSE_TAG, "Spacemouse movement - T:(%d, %d, %d)", tx, ty, tz);
+        
+        sendReport();
+    } else {
+        SQUID_LOG_DEBUG(SPACEMOUSE_TAG, "Spacemouse movement ignored - %s", 
+                     !isConnected() ? "not connected" : "no transport");
+    }
+}
+
+void SQUIDSPACEMOUSE::rotate(int16_t rx, int16_t ry, int16_t rz) {
+    if (isConnected() && transport) {
+        // Set rotation values
+        _rotReport.rx = rx;
+        _rotReport.ry = ry;
+        _rotReport.rz = rz;
+        
+        SQUID_LOG_DEBUG(SPACEMOUSE_TAG, "Spacemouse movement - R:(%d, %d, %d)", rx, ry, rz);
+        
+        sendReport();
+    } else {
+        SQUID_LOG_DEBUG(SPACEMOUSE_TAG, "Spacemouse movement ignored - %s", 
+                     !isConnected() ? "not connected" : "no transport");
+    }
+}
+
+void SQUIDSPACEMOUSE::press(uint8_t button) {
+    if (button < 1 || button > 32) {
+        SQUID_LOG_WARN(SPACEMOUSE_TAG, "Invalid button number: %d (must be 1-32)", button);
+        return;
+    }
+    
+    uint32_t buttonMask = (1UL << (button - 1));
+    _buttonReport.buttons |= buttonMask;
+    
+    SQUID_LOG_DEBUG(SPACEMOUSE_TAG, "Spacemouse button %d pressed - button state: 0x%08lX", 
+                 button, _buttonReport.buttons);
+    sendReport();
+}
+
+void SQUIDSPACEMOUSE::release(uint8_t button) {
+    if (button < 1 || button > 32) {
+        SQUID_LOG_WARN(SPACEMOUSE_TAG, "Invalid button number: %d (must be 1-32)", button);
+        return;
+    }
+    
+    uint32_t buttonMask = (1UL << (button - 1));
+    _buttonReport.buttons &= ~buttonMask;
+    
+    SQUID_LOG_DEBUG(SPACEMOUSE_TAG, "Spacemouse button %d released - button state: 0x%08lX", 
+                 button, _buttonReport.buttons);
+    sendReport();
+}
+
+bool SQUIDSPACEMOUSE::isPressed(uint8_t button) {
+    if (button < 1 || button > 32) {
+        return false;
+    }
+    
+    uint32_t buttonMask = (1UL << (button - 1));
+    bool pressed = (_buttonReport.buttons & buttonMask) != 0;
+    
+    SQUID_LOG_DEBUG(SPACEMOUSE_TAG, "Spacemouse button %d check - Pressed: %s", 
+                 button, pressed ? "true" : "false");
+    return pressed;
+}
+
+void SQUIDSPACEMOUSE::setAllButtons(uint32_t buttons) {
+    uint32_t previousButtons = _buttonReport.buttons;
+    _buttonReport.buttons = buttons;
+    
+    SQUID_LOG_DEBUG(SPACEMOUSE_TAG, "Spacemouse buttons set - previous: 0x%08lX, new: 0x%08lX", 
+                 previousButtons, buttons);
+    sendReport();
+}
+
+void SQUIDSPACEMOUSE::releaseAll() {
+    SQUID_LOG_DEBUG(SPACEMOUSE_TAG, "Releasing all Spacemouse buttons - previous state: 0x%08lX", 
+                 _buttonReport.buttons);
+    _buttonReport.buttons = 0;
+    sendReport();
+    SQUID_LOG_DEBUG(SPACEMOUSE_TAG, "All Spacemouse buttons released");
+}
+
+void SQUIDSPACEMOUSE::sendReport() {
+    if (!isConnected() || !transport) {
+        SQUID_LOG_DEBUG(SPACEMOUSE_TAG, "Cannot send Spacemouse report - not connected or no transport");
+        return;
+    }
+    
+    // Send translation report (Report ID 0x04)
+    bool transResult = transport->sendReport(SPACETRANS_ID, (uint8_t*)&_transReport, sizeof(_transReport));
+    
+    // Send rotation report (Report ID 0x05)  
+    bool rotResult = transport->sendReport(SPACEROTAT_ID, (uint8_t*)&_rotReport, sizeof(_rotReport));
+    
+    // Send button report (Report ID 0x06)
+    bool buttonResult = transport->sendReport(SPACECLICK_ID, (uint8_t*)&_buttonReport, sizeof(_buttonReport));
+    
+    if (!transResult || !rotResult || !buttonResult) {
+        SQUID_LOG_ERROR(SPACEMOUSE_TAG, "Failed to send Spacemouse reports - T:%s R:%s B:%s",
+                     transResult ? "OK" : "FAIL",
+                     rotResult ? "OK" : "FAIL", 
+                     buttonResult ? "OK" : "FAIL");
+    } else {
+        SQUID_LOG_DEBUG(SPACEMOUSE_TAG, "Spacemouse reports sent successfully");
+    }
+    
+    delay(_delay_ms);
+}
